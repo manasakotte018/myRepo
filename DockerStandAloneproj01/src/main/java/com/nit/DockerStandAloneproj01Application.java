8c481a73992cfe7e384b02d39f7ea72ba0832965
kottemanasa <Kottemanasa018@gmail.com>
2025-09-16 16:34:04 +0530

    intialchange

---
+++ DockerStandAloneproj01/src/main/java/com/nit/DockerStandAloneproj01Application.java
@@ -0,0 +1,14 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerStandAloneproj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerStandAloneproj01Application.class, args);
+		System.out.println("Welcome to Docker StandAlone");
+	}
+
+}
