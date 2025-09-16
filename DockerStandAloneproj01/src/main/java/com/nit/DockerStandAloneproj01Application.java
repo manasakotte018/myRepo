5a3486cad559b99f7472eefa599d9a334154677a
kottemanasa <Kottemanasa018@gmail.com>
2025-09-16 16:46:27 +0530

    second commit

--- DockerStandAloneproj01/src/main/java/com/nit/DockerStandAloneproj01Application.java
+++ DockerStandAloneproj01/src/main/java/com/nit/DockerStandAloneproj01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerStandAloneproj01Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerStandAloneproj01Application.class, args);
 		System.out.println("Welcome to Docker StandAlone");
